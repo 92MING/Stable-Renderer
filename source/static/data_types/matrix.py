d5740242fb4a2ce42f0f0a59a5d321781b352664
92MING <yashin.sd123@gmail.com>
2023-10-08 06:07:50 +0800

    * finished vector & matrix class * finished shader.setUniform

--- source/static/data_types/matrix.py
+++ source/static/data_types/matrix.py
@@ -3,137 +3,132 @@ from functools import partial
 from typing import Sequence
 from static.data_types.vector import Vector
 import warnings
+from utils.base_clses import SingleGenericClass
 
 def _getMatrix(type, *args):
-    if type == int:
-        warnings.warn('Note that int matrix is not supported for opengl uniform.')
-        type = np.int32
     return np.matrix(*args, dtype=type).view(Matrix)
 
-class Matrix(np.matrix):
+class Matrix(SingleGenericClass, np.matrix):
+    '''Default type is np.float32'''
+    DEFAULT_TYPE = np.float32
+
     def __new__(cls, *args, **kwargs):
-        '''When type is not specified, it will be inferred from the first element of args.'''
-        dtype = kwargs.get('dtype', None)
-        if dtype is None:
-            if not isinstance(args[0][0], Sequence):
-                args = [args,]
-            dtype = type(args[0][0][0])
-            if dtype ==float:
-                dtype = np.float32
-            elif dtype == int:
-                warnings.warn('Note that int matrix is not supported for opengl uniform.')
+        if len(args)>=2:
+            dtype = args[1]
+            if dtype == int:
                 dtype = np.int32
-        return np.matrix(*args, dtype=dtype).view(cls)
-    def __class_getitem__(cls, item):
-        if item == float:
-            item = np.float32
-        return partial(_getMatrix, item)
+            elif dtype == float:
+                dtype = np.float32
+            args = list(args)
+            args[1] = dtype
+            return np.array(*args, **kwargs).view(cls)
+        else:
+            dtype = kwargs.pop('dtype', cls.type())
+            return np.array(*args, dtype=dtype, **kwargs).view(cls)
+
+    @classmethod
+    def type(cls):
+        if cls._type is None:
+            if cls.DEFAULT_TYPE is not None:
+                return cls.DEFAULT_TYPE
+            raise TypeError(
+                f'{cls.__name__} has no generic type. Please use {cls.__name__}[type] to get a generic class')
+        if cls._type == float:
+            return np.float32
+        elif cls._type == int:
+            return np.int32
+        return cls._type
+
     @property
     def size(self):
         '''Return memory size in bytes'''
         return self.itemsize * super().size
 
-    @staticmethod
-    def Identity(n:int, type=np.float32):
-        return Matrix[type](np.identity(n))
+    @classmethod
+    def Identity(cls, n:int):
+        return cls(np.identity(n, dtype=cls.type()))
 
     # region transformation
-    @staticmethod
-    def RotationX(angle, radian=False):
+    @classmethod
+    def RotationX(cls, angle, radian=False):
         '''4x4 matrix'''
-        t = type(angle)
-        if t in (int, float):
-            t = np.float32
         if not radian:
             angle = np.radians(angle)
-        return Matrix[t]([[1, 0, 0, 0],
-                         [0, np.cos(angle), -np.sin(angle), 0],
-                         [0, np.sin(angle), np.cos(angle), 0],
-                         [0, 0, 0, 1]])
-    @staticmethod
-    def RotationY(angle, radian=False):
+        return cls([[1, 0, 0, 0],
+                     [0, np.cos(angle), -np.sin(angle), 0],
+                     [0, np.sin(angle), np.cos(angle), 0],
+                     [0, 0, 0, 1]], dtype=cls.type())
+    @classmethod
+    def RotationY(cls, angle, radian=False):
         '''4x4 matrix'''
-        t = type(angle)
-        if t in (int, float):
-            t = np.float32
         if not radian:
             angle = np.radians(angle)
-        return Matrix[t]([[np.cos(angle), 0, np.sin(angle), 0],
-                         [0, 1, 0, 0],
-                         [-np.sin(angle), 0, np.cos(angle), 0],
-                         [0, 0, 0, 1]])
-    @staticmethod
-    def RotationZ(angle, radian=False):
+        return cls([[np.cos(angle), 0, np.sin(angle), 0],
+                     [0, 1, 0, 0],
+                     [-np.sin(angle), 0, np.cos(angle), 0],
+                     [0, 0, 0, 1]], dtype=cls.type())
+    @classmethod
+    def RotationZ(cls, angle, radian=False):
         '''4x4 matrix'''
-        t = type(angle)
-        if t in (int, float):
-            t = np.float32
         if not radian:
             angle = np.radians(angle)
-        return Matrix[t]([[np.cos(angle), -np.sin(angle), 0, 0],
-                           [np.sin(angle), np.cos(angle), 0, 0],
-                           [0, 0, 1, 0],
-                           [0, 0, 0, 1]])
-    @staticmethod
-    def Rotation(x, y, z, radian=False):
+        return cls([[np.cos(angle), -np.sin(angle), 0, 0],
+                   [np.sin(angle), np.cos(angle), 0, 0],
+                   [0, 0, 1, 0],
+                   [0, 0, 0, 1]], dtype=cls.type())
+    @classmethod
+    def Rotation(cls, x, y, z, radian=False):
         '''rotation order is z, x, y. Note that this is 4x4 matrix'''
         if not radian:
             z, x, y = np.radians(z), np.radians(x), np.radians(y)
-        return Matrix.RotationZ(z, radian=radian) * Matrix.RotationX(x, radian=radian) * Matrix.RotationY(y, radian=radian)
+        return cls.RotationZ(z, radian=radian) * cls.RotationX(x, radian=radian) * cls.RotationY(y, radian=radian)
 
-    @staticmethod
-    def Translation(x, y, z):
-        t = type(x)
-        if t in (int, float):
-            t = np.float32
-        return Matrix[t]([[1, 0, 0, x],
-                          [0, 1, 0, y],
-                          [0, 0, 1, z],
-                          [0, 0, 0, 1]])
-    @staticmethod
-    def Scale(x, y, z):
-        t = type(x)
-        if t in (int, float):
-            t = np.float32
-        return Matrix[t]([[x, 0, 0, 0],
-                          [0, y, 0, 0],
-                          [0, 0, z, 0],
-                          [0, 0, 0, 1]])
+    @classmethod
+    def Translation(cls, x, y, z):
+        return cls([[1, 0, 0, x],
+                  [0, 1, 0, y],
+                  [0, 0, 1, z],
+                  [0, 0, 0, 1]], dtype=cls.type())
+    @classmethod
+    def Scale(cls, x, y, z):
+        return cls([[x, 0, 0, 0],
+                  [0, y, 0, 0],
+                  [0, 0, z, 0],
+                  [0, 0, 0, 1]], dtype=cls.type())
 
-    @staticmethod
-    def Transformation(translation, rotation, scale):
+    @classmethod
+    def Transformation(cls, translation, rotation, scale):
         '''TRS'''
-        t = Matrix.Translation(translation[0], translation[1], translation[2])
-        r = Matrix.Rotation(rotation[0], rotation[1], rotation[2])
-        s = Matrix.Scale(scale[0], scale[1], scale[2])
+        t = cls.Translation(translation[0], translation[1], translation[2])
+        r = cls.Rotation(rotation[0], rotation[1], rotation[2])
+        s = cls.Scale(scale[0], scale[1], scale[2])
         return t * r * s
     # endregion
 
-
     # region projection
-    @staticmethod
-    def Orthographic(left, right, bottom, top, near, far, type=np.float32):
-        return Matrix[type]([[2 / (right - left), 0, 0, -(right + left) / (right - left)],
-                             [0, 2 / (top - bottom), 0, -(top + bottom) / (top - bottom)],
-                             [0, 0, -2 / (far - near), -(far + near) / (far - near)],
-                             [0, 0, 0, 1]])
-    @staticmethod
-    def Perspective(fov, aspect, near, far, type=np.float32):
+    @classmethod
+    def Orthographic(cls, left, right, bottom, top, near, far):
+        return cls([[2 / (right - left), 0, 0, -(right + left) / (right - left)],
+                     [0, 2 / (top - bottom), 0, -(top + bottom) / (top - bottom)],
+                     [0, 0, -2 / (far - near), -(far + near) / (far - near)],
+                     [0, 0, 0, 1]], dtype=cls.type())
+    @classmethod
+    def Perspective(cls, fov, aspect, near, far):
         f = 1 / np.tan(np.radians(fov / 2))
-        return Matrix[type]([[f / aspect, 0, 0, 0],
-                             [0, f, 0, 0],
-                             [0, 0, (far + near) / (near - far), 2 * far * near / (near - far)],
-                             [0, 0, -1, 0]])
+        return cls([[f / aspect, 0, 0, 0],
+                     [0, f, 0, 0],
+                     [0, 0, (far + near) / (near - far), 2 * far * near / (near - far)],
+                     [0, 0, -1, 0]], dtype=cls.type())
     # endregion
 
     # region lookat
-    @staticmethod
-    def LookAt(eye, center, up, type=np.float32):
+    @classmethod
+    def LookAt(cls, eye, center, up):
         '''
         :param eye: eye position
         :param center: center position
         :param up: up vector
-        :return:
+        :return: 4x4 View matrix
         '''
         if isinstance(eye, (list, tuple)):
             eye = np.array(eye)
@@ -144,10 +139,10 @@ class Matrix(np.matrix):
         f = (center - eye).view(Vector).normalize
         s = f.cross(up).view(Vector).normalize
         u = s.cross(f)
-        return Matrix[type]([[s[0], s[1], s[2], -s.dot(eye)],
-                            [u[0], u[1], u[2], -u.dot(eye)],
-                            [-f[0], -f[1], -f[2], f.dot(eye)],
-                            [0, 0, 0, 1]])
+        return cls([[s[0], s[1], s[2], -s.dot(eye)],
+                    [u[0], u[1], u[2], -u.dot(eye)],
+                    [-f[0], -f[1], -f[2], f.dot(eye)],
+                    [0, 0, 0, 1]], dtype=cls.type())
     # endregion
 
 __all__ = ['Matrix']
--- source/utils/base_clses/generic_class.py
+++ source/utils/base_clses/generic_class.py
@@ -1,14 +1,19 @@
 # -*- coding: utf-8 -*-
 '''this module contains generic classes.'''
-from .cross_module_class import CrossModuleClass
 
-class SingleGenericClass(CrossModuleClass):
+class SingleGenericClass:
     _type = None
     _genericClses = {}
 
+    DEFAULT_TYPE = None
+    '''override this to set default type'''
+
     def __class_getitem__(cls, item):
+        if not isinstance(item, type):
+            raise TypeError(f'{cls.__name__} only accepts type as generic type')
         if item not in cls._genericClses:
-            newCls = type(f'{cls.__name__}{item}', (cls,), {'__slots__':()})
+            itemName = item.__qualname__.split('.')[-1] if hasattr(item, '__qualname__') else item.__name__.split('.')[-1]
+            newCls = type(f'{cls.__name__}_{itemName}', (cls,), {'__slots__':()})
             newCls._type = item
             cls._genericClses[item] = newCls
         return cls._genericClses[item]
@@ -16,16 +21,20 @@ class SingleGenericClass(CrossModuleClass):
     @classmethod
     def type(cls):
         if cls._type is None:
+            if cls.DEFAULT_TYPE is not None:
+                return cls.DEFAULT_TYPE
             raise TypeError(f'{cls.__name__} has no generic type. Please use {cls.__name__}[type] to get a generic class')
         return cls._type
 
-class MultiGenericClass(CrossModuleClass):
+class MultiGenericClass:
     _types = None
     _genericClses = {}
 
+    DEFAULT_TYPES = None
+    '''override this to set default types, e.g. (int, int)'''
+
     def __class_getitem__(cls, item):
-        if not isinstance(item, tuple):
-            item = (item,)
+        item = tuple(item)
         if item not in cls._genericClses:
             newCls = type(f'{cls.__name__}{item}', (cls,), {'__slots__':()})
             newCls._types = item
@@ -35,6 +44,8 @@ class MultiGenericClass(CrossModuleClass):
     @classmethod
     def types(cls):
         if cls._types is None:
+            if cls.DEFAULT_TYPES is not None:
+                return cls.DEFAULT_TYPES
             raise TypeError(f'{cls.__name__} has no generic types. Please use {cls.__name__}[types] to get a generic class')
         return cls._types
 
