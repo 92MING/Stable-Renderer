f7add48f972408b3f90d983073bc5b74268236f0
Euge <1507064225@qq.com>
2023-10-14 13:41:22 +0800

    updated

--- source/sd/sd_example.py
+++ source/sd/sd_example.py
@@ -2,11 +2,11 @@ import random
 import torch
 from PIL import Image
 from pathlib import Path
-from diffuser_pipeline.multi_frame_stable_diffusion import (
+from .modules import utils, config
+from .modules.diffuser_pipelines.multi_frame_stable_diffusion import (
     StableDiffusionImg2VideoPipeline,
     load_pipe,
 )
-from modules import utils, config
 
 
 def main():
