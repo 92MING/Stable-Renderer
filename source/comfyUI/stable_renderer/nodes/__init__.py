f9519743e9e1034fa8261cabf25c934ddd5836d7
ThinkThinkSyn <thinkthinksync@gmail.com>
2024-03-28 02:39:39 +0800

    Merge processing.py with processing/img.py

--- source/comfyUI/stable_renderer/nodes/__init__.py
+++ source/comfyUI/stable_renderer/nodes/__init__.py
@@ -7,29 +7,7 @@ from .samplers import *
 from .schedulers import *
 from .processing import *
 
-class TestNode:
-    @classmethod
-    def INPUT_TYPES(s):
-        return {
-                "required": {
-                    "x": ("INT", {"default": 0, "min": 0, "max": 128, "step": 1})
-                    },
-                }
-
-    OUTPUT_NODE = True
-    RETURN_TYPES = ("INT", "INT")
-    RETURN_NAMES = ("x", "y")
-    CATEGORY = "Stable-Renderer"
-    FUNCTION = "__call__"
-    
-    def __call__(self, x: int):
-        data = {"filename":"animate_diff_00004_.gif", "type": "output"}
-        return {"ui": {"images": [data, data], "animated": (True, True)}, "result": (x, x+1)}
-        
 
 # for ComfyUI's node registry
 NODE_CLASS_MAPPINGS = {cls.__qualname__: cls._RealComfyUINode for cls in NodeBase._AllSubclasses() if not cls._IsAbstract}
 NODE_DISPLAY_NAME_MAPPINGS = {cls.__qualname__: cls._ReadableName for cls in NodeBase._AllSubclasses() if not cls._IsAbstract}
-
-NODE_CLASS_MAPPINGS["TestNode"] = TestNode
-NODE_DISPLAY_NAME_MAPPINGS["TestNode"] = "Test Node"
