77ecc684155d5482ddf41487d43435c4aae0a019
Cyrus Wong <cyruss081115@gmail.com>
2024-04-22 16:03:23 +0800

    editing shader

--- source/engine/shaders/default_Gbuffer.frag.glsl
+++ source/engine/shaders/default_Gbuffer.frag.glsl
@@ -3,26 +3,31 @@
 #define MAX_LIGHTS_NUM 256  // this constant will be edit by python script
 #define RUNTIME_UBO_BINDING 0
 
+// runtime datastruct contains all runtime-update values
 layout (std140, binding=RUNTIME_UBO_BINDING) uniform Runtime {
 	mat4 model;
 	mat4 view;
 	mat4 projection;
 	mat4 MVP;
-	mat4 MVP_IT; // inverse transpose of MVP
-	mat4 MV; // model-view matrix
-	mat4 MV_IT; // inverse transpose of MV
+	mat4 MVP_IT; 	// inverse transpose of MVP
+	mat4 MV; 		// model-view matrix
+	mat4 MV_IT; 	// inverse transpose of MV
 	vec3 cameraPos;
 	vec3 cameraDir;
 	vec2 cameraNearFar;	// x=near, y=far
 	float cameraFov;
 };
 
-// depth is output directly from z-buffer
+// outputs
 layout (location = 0) out vec4 outColor; // rgba
-layout (location = 1) out ivec4 outID;  // outID = (objID, material id, uv_Xcoord, uv_Ycoord)
+layout (location = 1) out ivec4 outID;
+// `outID` has various usages:
+//		when normal render mode: outID = (objID, material id, uv_Xcoord, uv_Ycoord)
+//		when baking mode: outID = (3D pixel index, material id, uv_Xcoord, uv_Ycoord)
+
 layout (location = 2) out vec3 outPos; // global pos, output alpha for mask
-layout (location = 3) out vec4 out_normal_and_depth;	//normal (in view space),  output alpha for mask
-layout (location = 4) out vec4 outNoise; // noise, in latent shape
+layout (location = 3) out vec4 out_normal_and_depth;	//normal (in view space). depth is also outputed here.
+layout (location = 4) out vec4 outNoise; // noise, in latent shape 
 
 // textures
 uniform sampler2D diffuseTex;	// 0
@@ -50,12 +55,17 @@ uniform int hasNoiseTex;
 uniform int objID;
 uniform int materialID;
 
+//AI
+uniform int isBaking;	// whether in baking mode
+uniform int isAIMesh;	// whether it is rendering an AI-Mesh
+uniform int aiMesh_k;	// constant k, 3D-pixel baking count = k^2
+
 // from VS
 in vec3 modelPos;
 in vec3 worldPos;
-in vec3 modelNormal;	// not normal from normal map!
-in vec3 worldNormal;	// not normal from normal map!
-in vec3 viewNormal;		// not normal from normal map!
+in vec3 modelNormal;	// not normal from normal map, is from 3D mesh data
+in vec3 worldNormal;	// not normal from normal map, is from 3D mesh data
+in vec3 viewNormal;		// not normal from normal map, is from 3D mesh data
 in vec2 uv;
 in vec3 modelTangent;
 
@@ -81,19 +91,26 @@ void main() {
 	float depth = 1.0 - gl_FragCoord.z;	// reverse depth to make closer object as white
 
 	// get normal
+	vec3 real_model_normal = modelNormal;
 	if (hasNormalTex == 0){  // if no normal texture, use normal from mesh data
 		out_normal_and_depth = vec4(normalize(viewNormal) * 0.5 + 0.5, depth);
 	}
 	else{
 		vec3 bitangent = cross(modelNormal, modelTangent);
 		mat3 TBN = mat3(modelTangent, bitangent, modelNormal);
-		vec3 real_model_normal = normalize(texture(normalTex, uv).rgb * 2.0 - 1.0);
-		real_model_normal = normalize(TBN * real_model_normal);
+		real_model_normal = normalize(TBN * normalize(texture(normalTex, uv).rgb * 2.0 - 1.0));
 		vec3 real_view_normal = normalize(vec3(MV_IT * vec4(real_model_normal, 0.0)));
 		out_normal_and_depth = vec4(real_view_normal, depth);
 	}
 
 	// get id
 	ivec2 uvi = ivec2(uv * ivec2(textureSize(diffuseTex, 0)));
-	outID = ivec4(objID, materialID, uvi.x, uvi.y);
+	if (isBaking == 1){
+		// baking mode
+		vec3 posToCamDir = normalize(worldPos - cameraPos);
+		outID = ivec4(objID, materialID, uvi.x, uvi.y);
+	}
+	else{
+		outID = ivec4(objID, materialID, uvi.x, uvi.y);
+	}
 }
