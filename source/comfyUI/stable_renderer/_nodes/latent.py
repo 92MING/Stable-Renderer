ef79277de2aeda186c34445e1743eaa15f52446a
Cyrus Wong <cyruss081115@gmail.com>
2024-04-24 15:42:43 +0800

    Add runEngine scripts for sofa and sword

--- source/comfyUI/stable_renderer/_nodes/latent.py
+++ source/comfyUI/stable_renderer/_nodes/latent.py
@@ -14,7 +14,7 @@ class CorrMapLatentNoiseInitializer(StableRendererNodeBase):
                  batch_size: int,
                  seed: INT(0, 0xffffffffffffffff),
                  correspondence_map: CorrespondenceMap,) -> LATENT:
-        """Initialize latent noise from correspondence map"""
+        """Initialize both latent image and noise from correspondence map"""
         latent_generator = torch.manual_seed(seed)
         noise_generator = torch.manual_seed(seed + 1)
         corrMap_width, corrMap_height = correspondence_map.size
@@ -25,18 +25,15 @@ class CorrMapLatentNoiseInitializer(StableRendererNodeBase):
         noise = torch.randn([1, 4, corrMap_height, corrMap_width], device="cpu", generator=noise_generator)
         noise = noise.repeat(batch_size, 1, 1, 1)
         
-        printed = False
         for v_info in correspondence_map.Map.values():
             if len(v_info) == 1:
                 continue
             position_trace, frame_index_trace = zip(*v_info)
             y_position_trace, x_position_trace = zip(*position_trace)
+
             latent[frame_index_trace, :, y_position_trace, x_position_trace] = torch.randn(4)
             noise[frame_index_trace, :, y_position_trace, x_position_trace] = torch.randn(4)
-            if not printed:
-                print("Latent: ", latent[frame_index_trace, :, y_position_trace, x_position_trace])
-                print("Noise: ", noise[frame_index_trace, :, y_position_trace, x_position_trace])
-                printed = True
+
         latent = F.interpolate(latent, size=(height // 8, width // 8), mode="nearest")
         noise = F.interpolate(noise, size=(height // 8, width // 8), mode="nearest")
 
--- source/comfyUI/stable_renderer/_nodes/samplers.py
+++ source/comfyUI/stable_renderer/_nodes/samplers.py
@@ -28,17 +28,46 @@ class StableRenderSampler(StableRendererNodeBase):
                      "average", "frame_distance", "pixel_distance", "perpendicular_view_normal"
                  ] = "average",
                  noise_option: Literal['disable', 'default', 'incoming'] = 'default',
+                 apply_overlap_option: Literal['noise', 'denoised', 'both'] = 'noise',
                  noise_seed: INT(0, 0xffffffffffffffff)=0, # type: ignore
                  steps: INT(1, 10000)=20, # type: ignore
                  cfg: FLOAT(0.0, 100.0, 0.01, round=0.01)=8.0, # type: ignore
                  sampler_name: COMFY_SAMPLERS=_default_sampler,
                  scheduler: COMFY_SCHEDULERS=_default_scheduler,
                  denoise: FLOAT(0, 1) = 1.0)->LATENT:
+        """
+        This method is responsible for sampling with Correspondence Map.
+
+        Args:
+            model (MODEL): The model to render.
+            positive (CONDITIONING): The positive conditioning.
+            negative (CONDITIONING): The negative conditioning.
+            latent_image (LATENT): The latent image.
+            correspondence_map (CorrespondenceMap): The correspondence map.
+            alpha_scheduler (Scheduler): The alpha scheduler.
+            kernel_radius_scheduler (Scheduler): The kernel radius scheduler.
+            overlap_algorithm (Literal[
+                'average', 'frame_distance', 'pixel_distance', 'perpendicular_view_normal'
+            ], optional): The overlap algorithm. Defaults to "average".
+            noise_option (Literal['disable', 'default', 'incoming'], optional):
+                The noise option. Defaults to 'default'. If 'disable', noise is not used. 
+                If 'default', noise is used. If 'incoming', noise is used and the incoming noise is used.
+            apply_overlap_option (Literal['noise', 'denoised', 'both'], optional): The apply overlap option. Defaults to 'noise'.
+            noise_seed (INT(0, 0xffffffffffffffff), optional): The noise seed. Defaults to 0.
+            steps (INT(1, 10000), optional): The number of steps. Defaults to 20.
+            cfg (FLOAT(0.0, 100.0, 0.01, round=0.01), optional): The cfg value. Defaults to 8.0.
+            sampler_name (COMFY_SAMPLERS, optional): The sampler name. Defaults to _default_sampler.
+            scheduler (COMFY_SCHEDULERS, optional): The scheduler. Defaults to _default_scheduler.
+            denoise (FLOAT(0, 1), optional): The denoise value. Defaults to 1.0.
+
+        Returns:
+            LATENT: The sampled latent image.
+        """
         SUPPORTED_SAMPLERS = ["ddim", "ddpm"]
 
         if sampler_name not in SUPPORTED_SAMPLERS:
             ComfyUILogger.warning(f"Scheduling with {sampler_name} is not supported. Using default sampler instead.")
-            sampler_name = "ddim"
+            sampler_name = "ddpm"
 
         callbacks = []
         overlap = ResizeOverlap(
@@ -47,17 +76,11 @@ class StableRenderSampler(StableRendererNodeBase):
             algorithm=overlap_algorithm_factory(overlap_algorithm),
         )
 
-        def execute_overlap(context: SamplingCallbackContext):
-            # Modifies context.denoised in place
+        def execute_overlap_on_noise(context: SamplingCallbackContext):
+            # Modifies context.noise in place
             noise = context.noise
-            print("Noise shape", noise.shape)
-            print("Step index", context.step_index)
-            print("Total steps", context.total_steps)
             frame_seq = [frame.unsqueeze(0) for frame in noise]
-            print("frame shape", frame_seq[0].shape)
             estimated_denoising_timestep = 1000 - int(((context.step_index + 1) / context.total_steps) * 1000)
-            print("sigma", context.sigma)
-            print("Estimated Denoising step", estimated_denoising_timestep)
             overlapped_frame_seq = overlap.__call__(
                 frame_seq,
                 corr_map=correspondence_map,
@@ -66,6 +89,44 @@ class StableRenderSampler(StableRendererNodeBase):
             )
             for i, frame in enumerate(overlapped_frame_seq):
                 context.noise[i] = frame.squeeze()
+
+        def execute_overlap_on_denoised(context: SamplingCallbackContext):
+            # Modifies context.denoised in place
+            denoised = context.denoised
+            frame_seq = [frame.unsqueeze(0) for frame in denoised]
+            estimated_denoising_timestep = 1000 - int(((context.step_index + 1) / context.total_steps) * 1000)
+            overlapped_frame_seq = overlap.__call__(
+                frame_seq,
+                corr_map=correspondence_map,
+                step=context.step_index,
+                timestep=estimated_denoising_timestep
+            )
+            for i, frame in enumerate(overlapped_frame_seq):
+                context.denoised[i] = frame.squeeze()
+
+        def execute_overlap(context: SamplingCallbackContext):
+            match apply_overlap_option:
+                case 'noise':
+                    execute_overlap_on_noise(context)
+                case 'denoised':
+                    if sampler_name != "ddpm":
+                        ComfyUILogger.warning(
+                            f"apply_overlap_option is set to 'denoised' but sampler_name is not 'ddpm'. Using on noise version instead."
+                        )
+                        execute_overlap_on_noise(context)
+                    else:
+                        execute_overlap_on_denoised(context)
+                case 'both':
+                    if sampler_name != "ddpm":
+                        ComfyUILogger.warning(
+                            f"apply_overlap_option is set to 'both' but sampler_name is not 'ddpm'. Using on noise version instead."
+                        )
+                        execute_overlap_on_noise(context)
+                    else:
+                        execute_overlap_on_noise(context)
+                        execute_overlap_on_denoised(context)
+                case _:
+                    raise ValueError(f"Unknown apply_overlap_option: {apply_overlap_option}")
 
         callbacks.append(execute_overlap)
 
